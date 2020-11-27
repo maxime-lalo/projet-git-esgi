fdcd6a417679895b606a9ad28331649e9bd10891
Maxime LALO <maxime.lalo.pro@gmail.com>
2020-11-27 14:53:04 +0100

    Creating basic main that read the DTD

--- main.c
+++ main.c
@@ -4,4 +4,13 @@
 #include "functions/xmlFunctions.h"
 #include "functions/verifyDtd.h"
 
-int main(){}
+int main(){
+    //DTD
+    char* nomFichierDTD = "dtd1.dtd" ;
+    char** tabNameElement = malloc(sizeof(char*)*TAILLE_MAX);
+    char** tabAttributElement = malloc(sizeof(char*)*TAILLE_MAX);
+
+    int nbElementDTD = lireDTD(nomFichierDTD, tabNameElement, tabAttributElement);
+
+    return 0 ;
+}
